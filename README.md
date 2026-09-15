94bb969c8fe208940ece8d4008fd6b6b8ea06883
chen <notfoundhelen@gmail.com>
2026-09-15 12:52:08 +0800

    Initial customized website

---
+++ README.md
@@ -0,0 +1,9 @@
+# Birthday Page for a Friend
+灵感来自于[这个开源项目](https://github.com/houmahani/codrops-depth-gallery/)
+
+在原来的基础上修改了图片、色彩、文字
+
+一个再简单不过的生日礼物
+
+但是我做得很开心！
+
---
+++ src/data/galleryData.js
@@ -0,0 +1,76 @@
+import FLOWER01 from '@/assets/flower-01.webp'
+import FLOWER02 from '@/assets/flower-02.webp'
+import FLOWER03 from '@/assets/flower-03.webp'
+import FLOWER04 from '@/assets/flower-04.webp'
+import FLOWER05 from '@/assets/flower-05.webp'
+
+const galleryPlaneData = [
+  {
+    fallbackColor: '#feca4f',
+    accentColor: '#956f06',
+    textureSrc: FLOWER01,
+    position: { x: -0.9, y: 0 },
+    backgroundColor: '#000000',
+    blob1Color: '#a16741',
+    blob2Color: '#c9e0ff',
+    label: {
+      word: 'The emptiest of feelings…',
+      
+    },
+  },
+  {
+    fallbackColor: '#a9895f',
+    accentColor: '#fff5ee',
+    textureSrc: FLOWER02,
+    position: { x: 0.8, y: 0 },
+    backgroundColor: '#ac733e',
+    blob1Color: '#d6e6f5',
+    blob2Color: '#f8ca4c',
+    label: {
+      word: 'Do not go gentle into that good night',
+      
+    },
+  },
+  {
+    fallbackColor: '#f5fffa',
+    accentColor: '#afeeee',
+    textureSrc: FLOWER03,
+    position: { x: -0.7, y: 0 },
+    backgroundColor: '#dbbdcc',
+    blob1Color: '#a586b8',
+    blob2Color: '#afb4e4',
+    label: {
+      word: 'Let life be beautiful like summer flowers',
+      
+    },
+  },
+  {
+    fallbackColor: '#afeeee',
+    accentColor: '#afeeee',
+    textureSrc: FLOWER04,
+    position: { x: 1, y: 0 },
+    backgroundColor: '#c4d7eb',
+    blob1Color: '#fde8a3',
+    blob2Color: '#bfd9f3',
+    label: {
+      word: 'To strive, to seek, to find, and not to yield',
+      
+    },
+  },
+  {
+    fallbackColor: '#fefdfb',
+    accentColor: '#f1eadb',
+    textureSrc: FLOWER05,
+    position: { x: -0.7, y: 0 },
+    backgroundColor: '#fcf5ef',
+    blob1Color: '#fff4df',
+    blob2Color: '#f1f6ff',
+    label: {
+      word: 'Happy Birthday <3',
+      color: '#91836c',
+      
+    },
+  },
+]
+
+export { galleryPlaneData }
